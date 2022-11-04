e4d307a631ed44426da09d13e75d26aea15356e1
Yoyo <maonome@gmail.com>
2022-11-04 17:55:45 +0800

    edit style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
-    <h1>ABCSD</h1>
+  <div class="layout">
+
+    <div class="document">
+      <label for="upload">
+        請選擇文件
+        <input class="upload" id="upload" type="file" accept="image/*,.pdf" capture aria-label="file" ref="file" />
+      </label>
+      <p>{{ fileName }}</p>
+      <button type="button" class="active" @click="uploadFile">上傳簽署文件</button>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 
 export default defineComponent({
   components: {},
   setup() {
-    function base() {
-      return 1;
+    const file = ref();
+    const fileName = ref<string>('');
+    watch(() => file.value, () => {
+      if (file.value.files) {
+        // console.log(file.value.files[0].name);
+      }
+    });
+    function uploadFile() {
+      // console.log(file.value.files[0].name);
     }
-    return { base };
+    return { file, fileName, uploadFile };
   },
 });
 </script>
