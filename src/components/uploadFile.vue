ed77bb8223097ff99be5666339dbe659da3235ef
Yoyo <maonome@gmail.com>
2022-11-11 18:19:26 +0800

    pdf sec

--- src/components/uploadFile.vue
+++ src/components/uploadFile.vue
@@ -54,50 +54,47 @@ export default defineComponent({
 
     function handleFiles(event: Event): void {
       const fileItem = (event as any).target.files[0];
-      const fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(fileItem);
 
-      fileReader.onload = function () {
+      if (fileItem.type === "application/pdf") {
+        PDF(fileItem);
+      } else {
+        console.log(fileItem.type);
+      }
+    }
+    function PDF(file: any): void {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = () => {
         const loadingTask = pdfjs.getDocument({ data: fileReader.result });
 
-        loadingTask.promise.then(
-          function (pdf: any) {
-            console.log("PDF loaded");
-
-            // Fetch the first page
+        loadingTask.promise
+          .then((pdf) => {
             const pageNumber = 1;
-            pdf.getPage(pageNumber).then(function (page: any) {
-              console.log("Page loaded");
-
-              const scale = 0.3;
+            pdf.getPage(pageNumber).then((page) => {
+              const scale = 3;
               const viewport = page.getViewport({ scale });
-
-              // Prepare canvas using PDF page dimensions
               const canvas = document.getElementById(
                 "the-canvas"
               ) as HTMLCanvasElement;
-              if (canvas) {
-                const context = canvas.getContext("2d");
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Render PDF page into canvas context
-                const renderContext = {
-                  canvasContext: context,
-                  viewport,
-                };
-                const renderTask = page.render(renderContext);
-                renderTask.promise.then(function () {
-                  console.log("Page rendered");
-                });
-              }
+              const context = canvas.getContext("2d");
+              // canvas.height = viewport.height;
+              // canvas.width = viewport.width;
+              canvas.height = 500;
+              canvas.width = 200;
+              const renderContext = {
+                canvasContext: context,
+                viewport,
+              };
+              const renderTask = page.render(renderContext);
+              renderTask.promise.then(function () {
+                console.log("Page rendered");
+              });
             });
-          },
-          function (reason: any) {
-            // PDF loading error
-            console.error(reason);
-          }
-        );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       };
     }
 
